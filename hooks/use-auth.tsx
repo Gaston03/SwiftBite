8edfa9fdf2cae95bf2f361efdbd4f734bf0c9a85
Gaston Pascal tonguino <pgastong03@gmail.com>
2025-09-07 22:54:01 +0000

    minor changes

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error("usePayments must be used within a PaymentsProvider");
+    throw new Error("useAuth must be used within a AuthProvider");
   }
   return context;
 }
